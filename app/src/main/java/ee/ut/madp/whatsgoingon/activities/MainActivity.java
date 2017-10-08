0b2b8d593dcff3d4aa7eba1e28a53f53ad6d8650
DESKTOP-GMI5NRK\admin <pcendelinova@seznam.cz>
2017-10-08 15:38:23 +0300

    Nav drawer

--- app/src/main/java/ee/ut/madp/whatsgoingon/activities/MainActivity.java
+++ app/src/main/java/ee/ut/madp/whatsgoingon/activities/MainActivity.java
@@ -2,23 +2,35 @@ package ee.ut.madp.whatsgoingon.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
 import ee.ut.madp.whatsgoingon.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    CircleImageView profilePhoto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,28 +67,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -84,7 +74,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_chat) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_profile) {
 
         } else if (id == R.id.nav_events) {
@@ -101,4 +91,96 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @OnClick(R.id.fab)
+    void add() {
+
+    }
+
+
+    private void chooseFragment(int itemId) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        Fragment fragment = null;
+        Class fragmentClass = null;
+//        switch (itemId) {
+//            case R.id.nav_profile:
+//                fragmentClass = HomeFragment.class;
+//                break;
+//            case R.id.nav_inbox:
+//                Intent intent = new Intent(this, ConversationActivity.class);
+//                startActivityForResult(intent, CHAT_REQUEST_CODE);
+//                break;
+//            case R.id.nav_map:
+//                fragmentClass = MapsFragment.class;
+//                break;
+//            case R.id.nav_dog_breed:
+//                fragmentClass = DogBreedsFragment.class;
+//                break;
+//            case R.id.nav_reminder:
+//                fragmentClass = ReminderFragment.class;
+//                break;
+//            case R.id.nav_favourite_dog_breeds:
+//                fragmentClass = FavouriteDogBreedsFragment.class;
+//                break;
+//            case R.id.nav_settings:
+//                Intent intent2 = new Intent(MainActivity.this, SettingsActivity.class);
+//                startActivityForResult(intent2, SETTINGS_REQUEST_CODE);
+//                break;
+//            case R.id.nav_logout:
+//                ((FirebaseApplication) getApplication()).signOutUser(this);
+//                break;
+//            default:
+//                fragmentClass = HomeFragment.class;
+//
+//        }
+//
+//        try {
+//            fragment = (Fragment) fragmentClass.newInstance();
+//        } catch (Exception e) {
+//            Log.e(TAG, "Failed to obtain the fragment " + e.getMessage());
+//
+//        }
+
+        if (fragment != null && fragmentClass != null) {
+            fragmentManager.beginTransaction().replace(R.id.content_main, fragment).addToBackStack(null).commit();
+        }
+
+    }
+
+
+    /**
+     * It sets data for navigation drawer such as name, email and dogPhoto that can be taken from external storage or download from firebase storage
+     *
+     * @param name
+     * @param email
+     * @param photo
+     */
+    private void setupDataForDrawer(String name, String email, String photo) {
+        if (navigationView != null) {
+            View header = navigationView.getHeaderView(0);
+            profilePhoto = (CircleImageView) header.findViewById(R.id.user_photo);
+
+//            if (photo != null) {
+//                String imagePath = userId + STORAGE_IMAGES_PATH + STORAGE_MY_PROFILE_PHOTO;
+//                if (ImageUtils.checkExternalStorage() && ImageUtils.checkIfImageExits(MainActivity.this, imagePath)) {
+//                    File filePath = getExternalCacheDir();
+//                    File myDir = new File(filePath.getAbsolutePath() + "/" + imagePath);
+//                    Picasso.with(this).load(myDir).memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE).into(profilePhoto);
+//                } else {
+//                    if (photo.contains("https")) {
+//                        Picasso.with(this).load(photo).memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE).into(profilePhoto);
+//                    } else if (!photo.isEmpty()) {
+//                        profilePhoto.setImageBitmap(ImageUtils.decodeBitmap(photo));
+//                    }
+//                }
+//            }
+
+            TextView nameView = (TextView) header.findViewById(R.id.header_name);
+            nameView.setText(name);
+            TextView emailView = (TextView) header.findViewById(R.id.header_email);
+            emailView.setText(email);
+        }
+    }
+
 }
