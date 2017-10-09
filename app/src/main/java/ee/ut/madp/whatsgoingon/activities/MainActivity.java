d9c6c15169a3ba9ea0162bf990362afc14fc8894
nayriva <dondo21@outlook.sk>
2017-10-09 12:11:10 +0300

    fragments supoorted for chat and detail

--- app/src/main/java/ee/ut/madp/whatsgoingon/activities/MainActivity.java
+++ app/src/main/java/ee/ut/madp/whatsgoingon/activities/MainActivity.java
@@ -14,7 +14,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,9 +23,7 @@ import java.text.Normalizer;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ee.ut.madp.whatsgoingon.R;
-import ee.ut.madp.whatsgoingon.chat.ChannelsActivity;
 import ee.ut.madp.whatsgoingon.chat.ChatApplication;
-import ee.ut.madp.whatsgoingon.chat.ChatDetailActivity;
 import ee.ut.madp.whatsgoingon.fragments.ChatChannelsFragment;
 
 public class MainActivity extends AppCompatActivity
@@ -54,9 +51,18 @@ public class MainActivity extends AppCompatActivity
         firebaseAuth = FirebaseAuth.getInstance();
         setUpChannel();
 
+        setUpdNavigationView();
+        setUpDrawer();
+
+        navigationView.setCheckedItem(R.id.nav_chat);
+    }
+
+    private void setUpdNavigationView() {
         final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
 
+    private void setUpDrawer() {
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
@@ -66,12 +72,8 @@ public class MainActivity extends AppCompatActivity
                 drawer.requestLayout();
             }
         };
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-        //setupDrawerContent(navigationView);
-
-        navigationView.setCheckedItem(R.id.nav_chat);
     }
 
     private void setUpChannel() {
@@ -95,46 +97,18 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-//    private void setupDrawerContent(NavigationView navigationView) {
-//        navigationView.setNavigationItemSelectedListener(
-//                new NavigationView.OnNavigationItemSelectedListener() {
-//                    @Override
-//                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-//                        selectDrawerItem(menuItem);
-//                        return true;
-//
-//                    }
-//                });
-//    }
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         selectDrawerItem(item);
         return true;
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CHAT_CHANNELS_REQUEST_CODE) {
-            if (resultCode == RESULT_OK && data != null) {
-                String channelToJoin = data.getStringExtra("channelToJoin");
-                Intent intent = new Intent(this, ChatDetailActivity.class);
-                intent.putExtra("channelToJoin", channelToJoin);
-                startActivityForResult(intent, CHAT_DETAIL_REQUEST_CODE);
-            }
-        }
-    }
-
     public void selectDrawerItem(MenuItem menuItem) {
         Fragment fragment = null;
         Class fragmentClass = null;
-        Class activityClass = null;
-        int classRequestCode = -1;
-        switch(menuItem.getItemId()){
+        switch (menuItem.getItemId()) {
             case R.id.nav_chat:
                 fragmentClass = ChatChannelsFragment.class;
-                activityClass = ChannelsActivity.class;
-                classRequestCode = CHAT_CHANNELS_REQUEST_CODE;
                 break;
             case R.id.nav_events:
                 //fragmentClass = EventsFragment.class;
@@ -160,51 +134,22 @@ public class MainActivity extends AppCompatActivity
             fragment = (Fragment) fragmentClass.newInstance();
         } catch (Exception e) {
             Log.e(TAG, "Failed to obtain the fragment " + e.getMessage());
-
         }
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.containerView, fragment).commit();
-        startActivityForResult(new Intent(this, activityClass), classRequestCode);
 
         menuItem.setChecked(true);
         setTitle(menuItem.getTitle());
         drawerLayout.closeDrawers();
     }
 
-    private final int CHAT_CHANNELS_REQUEST_CODE = 1;
-    private final int CHAT_DETAIL_REQUEST_CODE = 2;
-    private final int EVENTS_REQUEST_CODE = 3;
-    private final int EVENT_DETAIL_REQUES_CODE = 4;
-    private final int EVENT_JOIN_REQUEST_CODE = 5;
-    private final int EVENT_ADD_REQUEST_CODE = 6;
-    private final int EVENT_EDIT_REQUEST_CODE = 7;
-    private final int SETTINGS_REQUEST_CODE = 8;
-    private final int HELP_REQUEST_CODE = 9;
-    private final int LOG_OUT_REQUEST_CODE = 10;
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        application.hostStopChannel();
-        application.quit();
-    }
-    /**
-     * Provides sign out of users
-     *
-     * @param context
-     */
     public void signOutUser(Context context) {
         FirebaseAuth.getInstance().signOut();
         LoginManager.getInstance().logOut();
+        application.hostStopChannel();
+        application.quit();
         context.startActivity(new Intent(context, LoginActivity.class));
-
-    }
-
-    /**
-     * Setups the navigation header, if data is not available from shared preferences and external storage, it downloads it from firebase database and storage.
-     */
-    public void setupNavigationHeader() {
 
     }
 }
--- app/src/main/java/ee/ut/madp/whatsgoingon/fragments/ChatFragment.java
+++ app/src/main/java/ee/ut/madp/whatsgoingon/fragments/ChatFragment.java
@@ -1,30 +1,141 @@
 package ee.ut.madp.whatsgoingon.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.text.Normalizer;
+import java.util.List;
+
+import butterknife.OnClick;
+import ee.ut.madp.whatsgoingon.Observable;
+import ee.ut.madp.whatsgoingon.Observer;
 import ee.ut.madp.whatsgoingon.R;
+import ee.ut.madp.whatsgoingon.chat.ChatApplication;
+import ee.ut.madp.whatsgoingon.chat.ChatMessageAdapter;
+import ee.ut.madp.whatsgoingon.models.ChatMessage;
 
-public class ChatFragment extends Fragment {
+public class ChatFragment extends Fragment implements Observer {
 
-
-    public ChatFragment() {
-        super();
-    }
+    private static final String TAG = "aa"; //TODO
+    private ChatApplication application;
+    private ListView messagesListView;
+    private ArrayAdapter<ChatMessage> adapter;
+    private EditText messageET;
+    private String channelToJoin;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        application = (ChatApplication) getActivity().getApplication();
+        application.addObserver(this);
+    }
+
+    public void setData(String data) {
+        channelToJoin = data;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        messagesListView = (ListView) view.findViewById(R.id.messagesListView);
+        adapter = new ChatMessageAdapter(getContext(), R.layout.message_list_item, application.getHistory());
+        messagesListView.setAdapter(adapter);
+
+        messageET = (EditText) view.findViewById(R.id.messageEditText);
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.sendFloatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+
+        joinChannel();
+        updateHistory();
+
+        return view;
     }
+
+    private void joinChannel() {
+        application.useSetChannelName(channelToJoin);
+        application.useJoinChannel();
+    }
+
+    private void updateHistory() {
+        Log.i(TAG, "updateHistory()");
+        adapter.clear();
+        List<ChatMessage> messages = application.getHistory();
+        for (ChatMessage message : messages) {
+            adapter.add(message);
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public synchronized void update(Observable o, Object arg) {
+        Log.i(TAG, "update(" + arg + ")");
+        String qualifier = (String)arg;
+
+        if (qualifier.equals(ChatApplication.APPLICATION_QUIT_EVENT)) {
+            Message message = handler.obtainMessage(HANDLE_APPLICATION_QUIT_EVENT);
+            handler.sendMessage(message);
+        }
+
+        if (qualifier.equals(ChatApplication.HISTORY_CHANGED_EVENT)) {
+            Message message = handler.obtainMessage(HANDLE_HISTORY_CHANGED_EVENT);
+            handler.sendMessage(message);
+        }
+
+        if (qualifier.equals(ChatApplication.USE_CHANNEL_STATE_CHANGED_EVENT)) {
+            Message message = handler.obtainMessage(HANDLE_CHANNEL_STATE_CHANGED_EVENT);
+            handler.sendMessage(message);
+        }
+
+        if (qualifier.equals(ChatApplication.ALLJOYN_ERROR_EVENT)) {
+            Message message = handler.obtainMessage(HANDLE_ALLJOYN_ERROR_EVENT);
+            handler.sendMessage(message);
+        }
+    }
+
+    public void sendMessage() {
+        Log.i(TAG, "sendMessage()");
+        String message = String.valueOf(messageET.getText());
+        application.newLocalUserMessage(message);
+        messageET.setText("");
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case HANDLE_HISTORY_CHANGED_EVENT: {
+                    Log.i(TAG, "mHandler.handleMessage(): HANDLE_HISTORY_CHANGED_EVENT");
+                    updateHistory();
+                    break;
+                } default:
+                    break;
+            }
+        }
+    };
+
+    private static final int HANDLE_APPLICATION_QUIT_EVENT = 0;
+    private static final int HANDLE_HISTORY_CHANGED_EVENT = 1;
+    private static final int HANDLE_CHANNEL_STATE_CHANGED_EVENT = 2;
+    private static final int HANDLE_ALLJOYN_ERROR_EVENT = 3;
 }
