b28f6e03058c8314fa50577b58f8e158942b4bf6
DESKTOP-GMI5NRK\admin <pcendelinova@seznam.cz>
2017-10-27 10:27:53 +0300

    New event activity

--- app/src/main/java/ee/ut/madp/whatsgoingon/fragments/EventFragment.java
+++ app/src/main/java/ee/ut/madp/whatsgoingon/fragments/EventFragment.java
@@ -1,12 +1,16 @@
 package ee.ut.madp.whatsgoingon.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ee.ut.madp.whatsgoingon.R;
+import ee.ut.madp.whatsgoingon.activities.NewEventActivity;
 
 public class EventFragment extends Fragment {
 
@@ -20,8 +24,14 @@ public class EventFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.btn_add_event)
+    public void showAddEventForm() {
+        startActivity(new Intent(getActivity(), NewEventActivity.class));
     }
 
 
