0f5b942165c1a5e1cb7c075a587b4631b7cbf15a
Nayriva <dondo21@outlook.sk>
2017-11-25 19:21:41 +0200

    .

--- app/src/main/java/ee/ut/madp/whatsgoingon/asynctasks/DeleteEventAsyncTask.java
+++ app/src/main/java/ee/ut/madp/whatsgoingon/asynctasks/DeleteEventAsyncTask.java
@@ -1,5 +1,7 @@
 package ee.ut.madp.whatsgoingon.asynctasks;
 
+import android.util.Log;
+
 import com.google.api.services.calendar.Calendar;
 
 import java.io.IOException;
@@ -7,19 +9,25 @@ import java.io.IOException;
 import ee.ut.madp.whatsgoingon.activities.EventFormActivity;
 
 /**
+ * Async task for deleting event.
  * Created by admin on 25.11.2017.
  */
 
 public class DeleteEventAsyncTask extends CalendarAsyncTask {
+
+    private static final String TAG = DeleteEventAsyncTask.class.getSimpleName();
+
     private String eventId;
 
     public DeleteEventAsyncTask(EventFormActivity activity, Calendar client, String googleEventId) {
         super(activity, client);
+        Log.i(TAG, "constructor");
         this.eventId = googleEventId;
     }
 
     @Override
     protected void doInBackground() throws IOException {
+        Log.i(TAG, "doInBackground");
         client.events().delete("primary", eventId).execute();
     }
 }
